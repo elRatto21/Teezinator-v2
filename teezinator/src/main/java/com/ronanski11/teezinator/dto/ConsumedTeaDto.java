7cddcbf48a64d35bbb067a193fe3321301d74bb6
Niklas Trapp <niklas031712@gmail.com>
2024-05-08 15:40:29 +0200

    copy old backend

---
+++ teezinator/src/main/java/com/ronanski11/teezinator/dto/ConsumedTeaDto.java
@@ -0,0 +1,23 @@
+package com.ronanski11.teezinator.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class ConsumedTeaDto {
+	
+	private String teaId;
+	
+	private String timeOfConsumption;
+	
+	private String image;
+	
+	private String imagePreview;
+	
+	private boolean sugar;
+
+	public boolean getSugar() {
+		return this.sugar;
+	}
+}
---
+++ teezinator/src/main/java/com/ronanski11/teezinator/model/ConsumedTea.java
@@ -0,0 +1,30 @@
+package com.ronanski11.teezinator.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Document
+@Data
+public class ConsumedTea {
+	
+	@Id
+	private String id;
+	
+	@DBRef
+	private Tea tea;
+	
+	private String user;
+	
+	private String image;
+	
+	private String preview;
+	
+	private LocalDateTime time;
+	
+	private boolean sugar;
+}
